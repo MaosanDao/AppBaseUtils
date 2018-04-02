bf5a1e1117fa40a3339a92f3b9d9520edb0e8359
Vangelis <460977141@qq.com>
2018-04-02 17:42:30 +0800

    Init Project

---
+++ appbaseutils/src/main/java/cn/vangelis/appbaseutils/AppActivityStackUtil.java
@@ -0,0 +1,190 @@
+package cn.vangelis.appbaseutils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Comment: Activity管理工具
+ *
+ * @author Vangelis.Wang in Make1
+ * @date 2018/4/2
+ * Email:Vangelis.wang@make1.cn
+ */
+
+public class AppActivityStackUtil {
+
+    private Application mContext;
+    public static final String TAG = "AppManager";
+    private static AppActivityStackUtil Instance = null;
+
+    /**
+     * 维护Activity 的list
+     */
+    private static Stack<Activity> mActivates = new Stack<>();
+
+
+    public static synchronized AppActivityStackUtil getInstance() {
+        if (Instance == null) {
+            return new AppActivityStackUtil();
+        }
+
+        return Instance;
+    }
+
+    /**
+     * 初始化 -- 需要在基类Application中使用
+     *
+     * @param context Application
+     */
+    public void init(Application context) {
+        mContext = context;
+        registerActivityLifecycleCallbacks();
+    }
+
+    /**
+     * 注册Activity的生命周期监听
+     */
+    private void registerActivityLifecycleCallbacks() {
+        mContext.registerActivityLifecycleCallbacks(new Application.ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                pushActivity(activity);
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+                if (null == mActivates || mActivates.isEmpty()) {
+                    return;
+                }
+                if (mActivates.contains(activity)) {
+                    //监听到 Activity销毁事件 将该Activity 从list中移除
+                    popActivity(activity);
+                }
+            }
+        });
+    }
+
+    /**
+     * 添加Activity
+     */
+    private void pushActivity(Activity activity) {
+        // 判断当前集合中不存在该Activity
+        Log.i(TAG, "pushActivity: ac:" + activity.getLocalClassName());
+        mActivates.add(activity);
+    }
+
+    /**
+     * 销毁单个Activity
+     */
+    private void popActivity(Activity activity) {
+        Log.i(TAG, "popActivity: ac:" + activity.getLocalClassName());
+        mActivates.remove(activity);
+    }
+
+    /**
+     * 销毁所有的Activity
+     */
+    public void removeALLActivity() {
+        if (mActivates == null) {
+            return;
+        }
+        for (Activity activity : mActivates) {
+            activity.finish();
+        }
+        mActivates.clear();
+    }
+
+    /**
+     * 获取栈顶的Activity
+     */
+    public Activity getTopActivity() {
+        return mActivates.lastElement();
+    }
+
+    /**
+     * 结束指定的Activity
+     *
+     * @param cls Activity
+     */
+    public void finishActivity(Class<?> cls) {
+        if (cls == null) {
+            return;
+        }
+        Iterator iterator = mActivates.iterator();
+        while (iterator.hasNext()) {
+            Activity activity = (Activity) iterator.next();
+            if (activity.getClass().equals(cls)) {
+                iterator.remove();
+                activity.finish();
+            }
+        }
+    }
+
+    /**
+     * 获取当前所有的Activity集合
+     */
+    public List<Activity> getAllActivity() {
+        if (mActivates.size() == 0) {
+            return null;
+        }
+        return mActivates.subList(0, mActivates.size());
+    }
+
+    /**
+     * 获取指定类的Activity
+     *
+     * @param cls 类名
+     * @return Activity
+     */
+    public Activity getActivity(Class<?> cls) {
+        if (cls == null) {
+            return null;
+        }
+        for (Activity activity1 : mActivates) {
+            if (activity1.getClass().equals(cls)) {
+                return activity1;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 退出app
+     */
+    public void exitApp() {
+        removeALLActivity();
+        System.exit(0);
+        android.os.Process.killProcess(android.os.Process.myPid());
+    }
+}
